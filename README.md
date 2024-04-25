fee21bbc84c21e437cb87b0eba23ba31920e1333
fenglielie <fenglielie@qq.com>
2024-04-26 01:08:49 +0800

    update

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ aeszero -e file_in -o file_out -k key
 
 可以使用长度为64字节的混淆密钥文件替代密钥字符串进行输入，此时需要提供混淆密钥文件的文件名，例如
 ```
-aeszero -e file_in -o file_out -w key_file
+aeszero -e file_in -o file_out -f key_file
 ```
 
 还可以直接不提供密钥或混淆密钥文件，此时程序会生成随机密钥
@@ -37,20 +37,33 @@ aeszero -d file_in -o file_out -k key
 
 和加密模式一样，可以使用长度为64字节的混淆密钥文件替代密钥字符串进行输入，此时需要提供混淆密钥文件的文件名，例如
 ```
-aeszero -d file_in -o file_out -w key_file
+aeszero -d file_in -o file_out -f key_file
 ```
 
 解密模式下如果不提供密钥或混淆密钥文件，程序会直接报错，
 
 使用示例
 ```
-Usage: aeszero -e|-d <infile> -k <keystring> | -w <mixkeyfile> -o <outfile>
+Usage: aeszero -e|-d <infile> -k <keystring> | -f <mixkeyfile> -o <outfile>
 Example:
 Encrypt (1): aeszero -e <infile> -o <outfile>
 Encrypt (2): aeszero -e <infile> -k <keystring> -o <outfile>
-Encrypt (3): aeszero -e <infile> -w <keyfile> -o <outfile>
+Encrypt (3): aeszero -e <infile> -f <keyfile> -o <outfile>
 Decrypt (1): aeszero -d <infile> -k <keystring> -o <outfile>
-Decrypt (2): aeszero -d <infile> -w <keyfile> -o <outfile>
+Decrypt (2): aeszero -d <infile> -f <keyfile> -o <outfile>
+```
+
+
+为了便于操作，支持单参数的智能模式：（智能模式下不会输出任何信息到控制台）
+
+- 如果输入的参数文件名以`.aes0`结尾，进入解密模式：记参数为`xxx.aes0`，推测密钥文件为`xxx.aes0.key`，推测输出文件为`xxx.aes0.dec`，相当于如下形式
+```
+aeszero -d xxx.aes0 -f xxx.aes0.key -o xxx.aes0.dec
+```
+
+- 否则进入加密模式：记参数为`xxx`，自动生成随机密钥，推测输出文件为`xxx.aes0`，相当于如下形式
+```
+aeszero -e xxx -o xxx.aes0
 ```
 
 
@@ -61,14 +74,15 @@ Decrypt (2): aeszero -d <infile> -w <keyfile> -o <outfile>
 3. 算法实际上采用了16字节的密钥，但是在导出时采用了简单的混淆处理，加入随机字符串使得导出64字节的混淆密钥。
 
 
-为了便于操作，支持单参数的智能模式：（智能模式下不会输出任何信息到控制台）
-
-- 如果输入的参数文件名以`.aes0`结尾，进入解密模式：记参数为`xxx.aes0`，推测密钥文件为`xxx.aes0.key`，推测输出文件为`xxx.aes0.dec`，相当于如下形式
+完整的选项支持如下，每一行的几个形式都是等价的
 ```
-aeszero -d xxx.aes0 -w xxx.aes0.key -o xxx.aes0.dec
+-e -E --encrypt
+-d -D --decrypt
+-k -K --key
+-f -F --keyfile
+-o -O --output
+-q -Q --quiet
+-v -V --version
+-h -H --help
 ```
-
-- 否则进入加密模式：记参数为`xxx`，自动生成随机密钥，推测输出文件为`xxx.aes0`，相当于如下形式
-```
-aeszero -e xxx -o xxx.aes0
-```
+其中`-q`选项用于关闭控制台输出，`-v`和`-h`输出版本信息和帮助。
--- src/aeszero_main.cpp
+++ src/aeszero_main.cpp
@@ -3,31 +3,43 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 struct ArgsType {
     char mode;
+    bool show_msg;
     std::string file_in_name;
     std::string file_out_name;
-    std::string key;
+    std::string mixkey;
 };
 
+bool oneof(const std::string &str, const std::vector<std::string> &str_set) {
+    for (const auto &s : str_set) {  // NOLINT(readability-use-anyofallof)
+        if (str == s) { return true; }
+    }
+    return false;
+}
+
+std::string version_str() { return "aeszero (version " VERSION_INFO ")\n"; }
+
 std::string usage_str() {
-    std::string str0{
-        "Usage: aeszero -e|-d <infile> -k <keystring> | -w <mixkeyfile> -o "
-        "<outfile>\nExample:\n"};
+    std::string usage = "Usage: aeszero -e|-d <infile> -k <keystring> | -f "
+                        "<mixkeyfile> -o <outfile>\n"
+                        "Example:\n";
 
-    std::string str1{"Encrypt (1): aeszero -e <infile> -o <outfile>\n"};
-    std::string str2{
-        "Encrypt (2): aeszero -e <infile> -k <keystring> -o <outfile>\n"};
-    std::string str3{
-        "Encrypt (3): aeszero -e <infile> -w <keyfile> -o <outfile>\n"};
+    std::string encrypt_examples =
+        "Encrypt (1): aeszero -e <infile> -o <outfile>\n"
+        "Encrypt (2): aeszero -e <infile> -k <keystring> -o <outfile>\n"
+        "Encrypt (3): aeszero -e <infile> -f <keyfile> -o <outfile>\n";
 
-    std::string str4{
-        "Decrypt (1): aeszero -d <infile> -k <keystring> -o <outfile>\n"};
-    std::string str5{
-        "Decrypt (2): aeszero -d <infile> -w <keyfile> -o <outfile>\n"};
+    std::string decrypt_examples =
+        "Decrypt (1): aeszero -d <infile> -k <keystring> -o <outfile>\n"
+        "Decrypt (2): aeszero -d <infile> -f <keyfile> -o <outfile>\n";
 
-    return str0 + str1 + str2 + str3 + str4 + str5;
+    std::string web = "For more information, please visit "
+                      "https://github.com/fenglielie/aeszero\n";
+
+    return usage + encrypt_examples + decrypt_examples + web;
 }
 
 void read_str_from_file(const std::string &file_name, std::string &str) {
@@ -54,7 +66,7 @@ ArgsType init_smart(const char *single_arg) {
     char mode = 0;
     std::string file_in_name;
     std::string file_out_name;
-    std::string key;
+    std::string mixkey;
     std::string arg1 = single_arg;
 
     auto ends_with = [](const std::string &str, const std::string &suffix) {
@@ -68,32 +80,32 @@ ArgsType init_smart(const char *single_arg) {
         file_in_name = arg1;
         file_out_name = file_in_name + ".dec";
         std::string key_file_name = file_in_name + ".key";
-        std::string mix_key_tmp;
-        read_str_from_file(key_file_name, mix_key_tmp);
-        key = AES0::InvMixkey(mix_key_tmp);
+        std::string mixkey_tmp;
+        read_str_from_file(key_file_name, mixkey_tmp);
+        mixkey = AES0::Mixkey(AES0::InvMixkey(mixkey_tmp));
     }
     else {  // mode == 'e'
         file_in_name = arg1;
         file_out_name = file_in_name + ".aes0";
-        key = AES0::Fixkey("");
-        std::string mix_key_str = AES0::Mixkey(key);
-        write_str_to_file(file_out_name + ".key", mix_key_str);
+        mixkey = AES0::Mixkey(AES0::Fixkey(""));
     }
 
-    return {mode, file_in_name, file_out_name, key};
+    return {mode, false, file_in_name, file_out_name, mixkey};
 }
 
+// NOLINTNEXTLINE
 ArgsType init(const int argc, const char *argv[]) {
     char mode = 0;
+    bool show_msg = true;
     std::string file_in_name;
     std::string file_out_name;
-    std::string key;
+    std::string mixkey;
 
     for (int index = 1; index < argc; index++) {
         std::string arg = argv[index];
 
-        // 检查参数是否为模式标志（-e 或 -d）
-        if (arg == "-e" || arg == "-d") {
+        // 检查参数是否为加密模式标志
+        if (oneof(arg, {"-e", "-E", "--encrypt"})) {
             if (mode != 0) {
                 std::cerr << "Error: mode already set " << std::endl;
                 exit(1);
@@ -104,12 +116,27 @@ ArgsType init(const int argc, const char *argv[]) {
                 exit(1);
             }
 
-            mode = arg[1];
+            mode = 'e';
             file_in_name = argv[++index];
         }
-        // 检查参数是否为密钥字符串标志（-k）
-        else if (arg == "-k") {
-            if (key != "") {
+        // 检查参数是否为解密模式标志
+        else if (oneof(arg, {"-d", "-D", "--decrypt"})) {
+            if (mode != 0) {
+                std::cerr << "Error: mode already set " << std::endl;
+                exit(1);
+            }
+            if (index + 1 >= argc) {
+                std::cerr << "Error: Missing input file path after " << arg
+                          << std::endl;
+                exit(1);
+            }
+
+            mode = 'd';
+            file_in_name = argv[++index];
+        }
+        // 检查参数是否为密钥字符串标志
+        else if (oneof(arg, {"-k", "-K", "--key"})) {
+            if (mixkey != "") {
                 std::cerr << "Error: key already set " << std::endl;
                 exit(1);
             }
@@ -119,11 +146,11 @@ ArgsType init(const int argc, const char *argv[]) {
             }
 
             std::string key_tmp = argv[++index];
-            key = AES0::Fixkey(key_tmp);
+            mixkey = AES0::Mixkey(AES0::Fixkey(key_tmp));
         }
-        // 检查参数是否为扩展密钥文件标志（-w）
-        else if (arg == "-w") {
-            if (key != "") {
+        // 检查参数是否为扩展密钥文件标志
+        else if (oneof(arg, {"-f", "-F", "--keyfile"})) {
+            if (mixkey != "") {
                 std::cerr << "Error: key already set " << std::endl;
                 exit(1);
             }
@@ -135,10 +162,10 @@ ArgsType init(const int argc, const char *argv[]) {
             std::string key_file_name = argv[++index];
             std::string mix_key_tmp;
             read_str_from_file(key_file_name, mix_key_tmp);
-            key = AES0::InvMixkey(mix_key_tmp);
+            mixkey = AES0::Mixkey(AES0::InvMixkey(mix_key_tmp));
         }
-        // 检查参数是否为输出文件标志（-o）
-        else if (arg == "-o") {
+        // 检查参数是否为输出文件标志
+        else if (oneof(arg, {"-o", "-O", "--output"})) {
             if (file_out_name != "") {
                 std::cerr << "Error: output file already set " << std::endl;
                 exit(1);
@@ -151,6 +178,8 @@ ArgsType init(const int argc, const char *argv[]) {
             }
             file_out_name = argv[++index];
         }
+        // 关闭输出信息
+        else if (oneof(arg, {"-q", "-Q", "--quiet"})) { show_msg = false; }
         // 其他参数均无效
         else {
             std::cerr << "Error: Invalid argument " << arg << std::endl;
@@ -160,69 +189,67 @@ ArgsType init(const int argc, const char *argv[]) {
 
     // 检查是否提供了模式、文件路径和密钥字符串
     if (mode == 0 || file_in_name.empty() || file_out_name.empty()) {
-        std::cerr << "Error: Missing required arguments.\n";
-        std::cerr << usage_str();
+        std::cerr << "Error: Missing required arguments.\n" << usage_str();
         exit(1);
     }
 
-    if (key.empty()) {
+    if (mixkey.empty()) {
         if (mode == 'd') {
             // 解密模式下不允许密钥为空
-            std::cerr << "Error: Missing key string.\n";
-            std::cerr << usage_str();
+            std::cerr << "Error: Missing key string.\n" << usage_str();
             exit(1);
         }
         // 加密模式下允许密钥为空，此时自动生成随机密钥
-        key = AES0::Fixkey({});
+        mixkey = AES0::Mixkey(AES0::Fixkey({}));
     }
 
-    // 在加密模式下，将扩充后的密钥mixkey自动存储在文件中
-    if (mode == 'e') {
-        std::string mix_key_str = AES0::Mixkey(key);
-        write_str_to_file(file_out_name + ".key", mix_key_str);
-    }
-
-    return {mode, file_in_name, file_out_name, key};
+    return {mode, show_msg, file_in_name, file_out_name, mixkey};
 }
 
-void run(const ArgsType &args, bool show_msg) {
-    if (show_msg) {
+void run(const ArgsType &args) {
+    if (args.show_msg) {
         if (args.mode == 'e') { std::cout << "[Encrypting]\n"; }
         else { std::cout << "[Decrypting]\n"; }
 
         std::cout << "Input file: " << args.file_in_name << std::endl;
         std::cout << "Output file: " << args.file_out_name << std::endl;
-        std::cout << "Key: " << args.key << std::endl;
+        std::cout << "Key: " << args.mixkey << std::endl;
     }
 
     if (args.mode == 'e') {
-        AES0::FileEncrypt(args.file_in_name, args.file_out_name, args.key);
+        AES0::FileEncrypt(args.file_in_name, args.file_out_name,
+                          AES0::InvMixkey(args.mixkey));
+        write_str_to_file(args.file_out_name + ".key", args.mixkey);
     }
     else {  // args.mode == 'd'
-        AES0::FileDecrypt(args.file_in_name, args.file_out_name, args.key);
+        AES0::FileDecrypt(args.file_in_name, args.file_out_name,
+                          AES0::InvMixkey(args.mixkey));
     }
 }
 
 int main(int argc, const char *argv[]) {
-    // int main() {
-    //     // 自定义命令行参数数量
-    //     int argc = 7;
-    //     // 自定义命令行参数数组
-    //     const char *argv[] = {"eaeszero", "-e", "image.png",    "-k",
-    //                           "12345",    "-o", "image2.png"};
-
     if (argc == 1) {
-        std::cout << usage_str();
+        std::cout << version_str() << usage_str();
         return 0;
     }
 
     if (argc == 2) {
+        if (oneof(argv[1], {"-v", "-V", "--version"})) {
+            std::cout << version_str();
+            return 0;
+        }
+
+        if (oneof(argv[1], {"-h", "-H", "--help"})) {
+            std::cout << usage_str();
+            return 0;
+        }
+
         auto args = init_smart(argv[1]);
-        run(args, false);
+        run(args);
     }
     else {
         auto args = init(argc, argv);
-        run(args, true);
+        run(args);
     }
 
     return 0;
